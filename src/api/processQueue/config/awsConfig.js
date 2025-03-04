44396b0b0602fc60b4b58a1dfa05a9a367fab2cd
eswardev nekkanti <eswardev.nekkanti@cognizant.com>
2025-03-04 11:40:21 +0000

    changes for testing credentials after package removal

--- src/api/processQueue/config/awsConfig.js
+++ src/api/processQueue/config/awsConfig.js
@@ -1,12 +1,10 @@
 import { SQSClient } from '@aws-sdk/client-sqs'
 import { config } from '~/src/config/index.js'
-import { fromIni } from '@aws-sdk/credential-providers'
 
 const awsRegion = config.get('awsRegion')
 const awsEndPoint = config.get('awsSQSEndPoint')
 
 export const sqsClient = new SQSClient({
   region: awsRegion,
-  endpoint: awsEndPoint,
-  credentials: fromIni()
+  endpoint: awsEndPoint
 })
