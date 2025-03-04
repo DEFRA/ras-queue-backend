d808fe4fe3fd17923b61067a325c3fa70c07f4af
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2025-03-04 13:20:20 +0000

    reverts sqs clients, removes region/endpoint cfg, adds getUrl test

--- src/api/processQueue/config/awsConfig.js
+++ src/api/processQueue/config/awsConfig.js
@@ -1,10 +1,7 @@
 import { SQSClient } from '@aws-sdk/client-sqs'
-import { config } from '~/src/config/index.js'
+// import { config } from '~/src/config/index.js'
 
-const awsRegion = config.get('awsRegion')
-const awsEndPoint = config.get('awsSQSEndPoint')
+// const awsRegion = config.get('awsRegion')
+// const awsEndPoint = config.get('awsSQSEndPoint')
 
-export const sqsClient = new SQSClient({
-  region: awsRegion,
-  endpoint: awsEndPoint
-})
+export const sqsClient = new SQSClient({})
